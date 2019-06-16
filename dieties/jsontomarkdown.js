2bf29cb6f3cdf26251f5fbb7fa05df77fb7d01c4
acc1729 <acc1729@gmail.com>
2019-06-15 21:05:19 -0400

    Add json-to-markdown node script

---
+++ dieties/jsontomarkdown.js
@@ -0,0 +1,33 @@
+'use strict';
+var fs = require('fs');
+;
+function jsonToMarkdown(diety) {
+    if (diety.name.startsWith("!"))
+        return;
+    var spheres = [];
+    for (var _i = 0, _a = diety.spheres; _i < _a.length; _i++) {
+        var sphere = _a[_i];
+        if (sphere.startsWith("!"))
+            continue;
+        spheres.push(sphere);
+    }
+    ;
+    var descriptions = [];
+    for (var _b = 0, _c = diety.descriptions; _b < _c.length; _b++) {
+        var description = _c[_b];
+        if (description.startsWith("!"))
+            continue;
+        descriptions.push(description);
+    }
+    ;
+    return ("###" + diety.name + "\n##Spheres: " + spheres.join(", ") + "\n\n" + descriptions.join("\n") + "\n");
+}
+;
+var files = fs.readdirSync('./').filter(function (file) { return file.endsWith('.json'); });
+var payload = [];
+files.forEach(function (file) {
+    var rawdata = fs.readFileSync(file).toString();
+    var diety = JSON.parse(rawdata);
+    payload.push(jsonToMarkdown(diety));
+});
+fs.writeFileSync('dieties.md', payload.join('\n'));
---
+++ dieties/jsontomarkdown.ts
@@ -0,0 +1,41 @@
+'use strict';
+
+const fs = require('fs');
+
+interface Diety {
+    name: string;
+    descriptions: Array<string>;
+    spheres: Array<string>;
+    adherents: Array<string> | null;
+};
+
+function jsonToMarkdown(diety: Diety): string | void {
+    if (diety.name.startsWith("!")) return;
+    let spheres = [];
+    for (let sphere of diety.spheres) {
+        if (sphere.startsWith("!")) continue;
+        spheres.push(sphere);
+    };
+    let descriptions = [];
+    for (let description of diety.descriptions) {
+        if (description.startsWith("!")) continue;
+        descriptions.push(description);
+    };
+    return (
+        `###${diety.name}
+##Spheres: ${spheres.join(", ")}
+
+${descriptions.join("\n")}
+`
+    );
+};
+
+let files: Array<string> = fs.readdirSync('./').filter((file: string): boolean => { return file.endsWith('.json') });
+let payload: Array<string | void> = [];
+files.forEach(file => {
+    let rawdata = fs.readFileSync(file).toString();
+    let diety = JSON.parse(rawdata);
+    payload.push(jsonToMarkdown(diety));
+});
+
+fs.writeFileSync('dieties.md', payload.join('\n'));
