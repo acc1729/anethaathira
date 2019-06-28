f0434103685b1b3637b5cab4d485534cd0f96892
acc1729 <acc1729@gmail.com>
2019-06-27 22:35:55 -0400

    Add converter for countries

---
+++ places/states/countrytomarkdown.js
@@ -0,0 +1,49 @@
+'use strict';
+exports.__esModule = true;
+var globber_1 = require("./../../src/globber");
+;
+var options = {
+    hide: process.argv[2] === "true" ? true : false,
+    categories: process.argv.slice(3)
+};
+function jsonToCountry(country, options) {
+    var hide = options.hide, categories = options.categories;
+    if (hide && country.name.startsWith("!"))
+        return;
+    var cities = [];
+    for (var _i = 0, _a = country.cities; _i < _a.length; _i++) {
+        var city = _a[_i];
+        if (hide && city.startsWith("!"))
+            continue;
+        cities.push(city);
+    }
+    var languages = [];
+    for (var _b = 0, _c = country.languages; _b < _c.length; _b++) {
+        var language = _c[_b];
+        if (hide && language.startsWith("!"))
+            continue;
+        languages.push(language);
+    }
+    ;
+    var biomes = [];
+    for (var _d = 0, _e = country.biomes; _d < _e.length; _d++) {
+        var biome = _e[_d];
+        if (hide && biome.startsWith("!"))
+            continue;
+        biomes.push(biome);
+    }
+    ;
+    var descriptions = [];
+    for (var _f = 0, _g = country.descriptions; _f < _g.length; _f++) {
+        var description = _g[_f];
+        if (hide && description.startsWith("!"))
+            continue;
+        descriptions.push(description);
+    }
+    ;
+    var return_string = "";
+    return_string.concat("## " + country.name);
+    return ("## " + country.name + "\n\n### Common languages: " + languages.join(", ") + "\n### Present biomes: " + biomes.join(", ") + "s\n\n" + descriptions.join("  s\n") + "\n");
+}
+;
+globber_1["default"](jsonToCountry, options, "# Countries of Anethaathira\n", 'country.md');
---
+++ places/states/countrytomarkdown.ts
@@ -0,0 +1,60 @@
+'use strict';
+
+import gatherAndPrint from './../../src/globber'
+
+interface Country {
+    name: string;
+    languages: Array<string>;
+    cities: Array<string>;
+    biomes: Array<string>;
+    descriptions: Array<string>;
+};
+
+interface Options {
+    hide: boolean,
+    categories: Array<string>
+}
+
+const options: Options = {
+    hide: process.argv[2] === "true" ? true : false,
+    categories: process.argv.slice(3),
+}
+
+function jsonToCountry(country: Country, options: Options): string | void {
+    const { hide, categories } = options;
+    if (hide && country.name.startsWith("!")) return;
+    let cities = [];
+    for (let city of country.cities) {
+        if (hide && city.startsWith("!")) continue;
+        cities.push(city);
+    }
+    let languages = [];
+    for (let language of country.languages) {
+        if (hide && language.startsWith("!")) continue;
+        languages.push(language);
+    };
+    let biomes = [];
+    for (let biome of country.biomes) {
+        if (hide && biome.startsWith("!")) continue;
+        biomes.push(biome);
+    };
+    let descriptions = [];
+    for (let description of country.descriptions) {
+        if (hide && description.startsWith("!")) continue;
+        descriptions.push(description);
+    };
+    let return_string = "";
+    return_string.concat(`## ${country.name}`);
+
+    return (
+        `## ${country.name}
+
+### Common languages: ${languages.join(", ")}
+### Present biomes: ${biomes.join(", ")}s
+
+${descriptions.join("  s\n")}
+`
+    );
+};
+
+gatherAndPrint(jsonToCountry, options, "# Countries of Anethaathira\n", 'country.md');
